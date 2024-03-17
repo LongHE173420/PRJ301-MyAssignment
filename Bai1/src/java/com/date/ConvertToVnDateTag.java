dd159984e4a7579af626e5a0da53f17497a0d759
LongHE173420 <162280638+LongHE173420@users.noreply.github.com>
2024-03-17 21:41:22 +0700

    Bai1

---
+++ Bai1/src/java/com/date/ConvertToVnDateTag.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.date;
+
+/**
+ *
+ * @author Admin
+ */
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+
+public class ConvertToVnDateTag extends SimpleTagSupport {
+    private String value;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        JspWriter out = getJspContext().getOut();
+        
+       
+
+        try {
+             SimpleDateFormat inputFormat = new SimpleDateFormat("MM-dd-yyyy");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("'ngay' dd 'thang' MM 'nam' yyyy");
+            Date date = inputFormat.parse(value);
+            String formattedDate = outputFormat.format(date);
+            out.print(formattedDate);
+        } catch (ParseException e) {
+            throw new JspException("Error: " + e.getMessage());
+        }
+    }
+}
+
---
+++ Bai1/web/Date.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : Date
+    Created on : Mar 17, 2024, 9:12:11 PM
+    Author     : Admin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="mytag" uri="WEB-INF/custom.tld"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+          <mytag:date value="05-15-2024"/>
+    </body>
+</html>
